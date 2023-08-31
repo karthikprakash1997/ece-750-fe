08702b3cfd630a5a17974227aa1dffbb3ffa8a0a
Shreyas-BS <bsshreyasbs@gmail.com>
2023-08-31 16:26:57 -0400

    [4] - Added async thunk

--- src/slices/overview/index.js
+++ src/slices/overview/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { apiCall } from '../../utils/common'; // Assuming you have your API utility function
 
 const initialState = {
   isMapTopologyFetching: false,
@@ -7,28 +8,50 @@ const initialState = {
   error: null,
 };
 
+// Async thunk for fetching map topology data
+export const fetchMapTopologyData = createAsyncThunk(
+  'overview/fetchMapTopologyData',
+  async () => {
+    try {
+      const response = await apiCall({
+        url: 'https://code.highcharts.com/mapdata/custom/world.topo.json',
+        action: 'GET',
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
 const overviewSlice = createSlice({
   name: 'overview',
   initialState,
-  reducers: {
-    mapTopologyRequest: (state) => {
-      state.isMapTopologyFetching = true;
-      state.isMapTopologyError = false;
-      state.error = null;
-    },
-    mapTopologyResponse: (state, action) => {
-      state.isMapTopologyFetching = false;
-      state.mapTopology = action.payload.data;
-      state.isMapTopologyError = false;
-      state.error = null;
-    },
-    mapTopologyRequestFail: (state, action) => {
-      state.isMapTopologyFetching = false;
-      state.isMapTopologyError = true;
-      state.error = action.payload.error;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMapTopologyData.pending, (state) => {
+        state.isMapTopologyFetching = true;
+        state.isMapTopologyError = false;
+        state.error = null;
+      })
+      .addCase(fetchMapTopologyData.fulfilled, (state, action) => {
+        state.isMapTopologyFetching = false;
+        state.mapTopology = action.payload;
+        state.isMapTopologyError = false;
+        state.error = null;
+      })
+      .addCase(fetchMapTopologyData.rejected, (state, action) => {
+        state.isMapTopologyFetching = false;
+        state.isMapTopologyError = true;
+        state.error = action.error.message;
+      });
   },
 });
 
-export const overviewActions = overviewSlice.actions;
+export const overviewActions = {
+  ...overviewSlice.actions,
+  fetchMapTopologyData,
+};
+
 export default overviewSlice.reducer;
