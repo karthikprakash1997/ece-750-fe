67a28e68f3d10742e8ae63bc35399d898e263d24
Karthik Prakash Sivakumar <karthiksiva1997@gmail.com>
2023-10-14 01:49:51 -0400

    added code linters

--- src/utils/helpers/report.js
+++ src/utils/helpers/report.js
@@ -4,7 +4,7 @@ export const countryCodeConversion = (data) => {
   return data.map((it) => {
     return {
       ...it,
-      countries: it.countries.map(country=> COUNTRY_DETAILS.find((dataum) => dataum.alpha2 === country)?.country ||"Null")
+      countries: it.countries.map(country=> countryCodeTocountries.find((dataum) => dataum.code === country)?.country ||"Null")
     };
   });
 };
